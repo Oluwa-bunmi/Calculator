7724665b44b6c2d16a0baf5747c3b8b15e1ed2d2
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-04-21 09:48:10 +0100

    still working

---
+++ main.js
@@ -0,0 +1,30 @@
+let result = document.getElementById("inputText");
+let calculate = (number) =>{
+    result.value +=  number;
+}
+let Result = ()=>{
+    try{
+        result.value = eval(result.value)
+    }
+    catch(err){
+        alert('Syntax Error')
+    }
+}
+function clr(){
+    result.value = " ";
+}
+function del(){
+    result.value = result.value.slice(0,-1);
+}
+function sin(){
+    result.value = Math.sin(result.value)  ;
+}
+function cos(){
+result.value = Math.cos(result.value);
+}
+function tan(){
+result.value = Math.tan(result.value);
+}
+function log(){
+result.value = Math.log10(result.value);
+}
